bb6c1d8f0020f54099bcc6f3604a3c780c3b411c
hendarto-6226 <hsprasetio@gmail.com>
2026-05-06 23:11:08 +0700

    analisa dg summary comment

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,43 @@
 //   4. Uji fungsi searchBook dengan dan tanpa parameter
 // Silakan bereksplorasi untuk memastikan semua fungsi berjalan dengan baik
 
+// Function Start BMA
+// Function TampilMenu 
+// 1-Function userToAddBook
+//    Function addBook (from userToAddBook to        
+//    addBook in bookManager 
+
+
+
 console.log("Book Management Application - Week 6");
 console.log("=====================================");
 
 // Mulai pengujian di bawah ini
 
+const prompt = require('prompt-sync')();
+import { addBook, listBooks, searchBook } from './functions/bookManager';
+import { Book } from './types/index'; 
+
+
+function menuPilihan(): void {
+  console.log("\nBook Management Application:");
+  console.log("1. Tambah Buku");
+  console.log("2. Tampilkan Semua Buku");
+  console.log("3. Cari Buku Berdasarkan Judul");
+  console.log("4. Keluar");
+  console.log("=====================================");
+}
+
+Function AddBook(book: Book): void {
+  console.log(`Menambahkan buku: `);
+  const title = prompt("Masukkan judul buku: ");
+  const author = prompt("Masukkan nama penulis: ");
+  const publicationYear = parseInt(prompt("Masukkan tahun publikasi: "));
+  
+  // Validasi input
+  if (!title || !author || isNaN(publicationYear)) {
+    console.log("Input tidak valid. Pastikan semua field diisi dengan benar.");
+    return;
+  }
+
+  
